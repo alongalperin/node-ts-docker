04c67c75d52bd9caf162e1da52f9db9896da402f
Alon <alonga90@gmail.com>
2021-04-03 17:38:08 +0300

    before github upload

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('hhhii');
+  res.send('Berg 2021');
 });
 
 app.listen(process.env.PORT || 5000, () => {
