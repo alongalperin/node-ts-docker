73367441c8451fb1e2beb79357bb2639bb688423
alon galperin <alonga90@gmail.com>
2021-04-09 12:05:36 +0300

    remove nodemon

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const PORT = 4000;
+
 app.get("/", (req, res) => {
-  res.send('Berg 2021');
+  res.send('Berg 2021!');
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log('server running on port 4000 ');
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
