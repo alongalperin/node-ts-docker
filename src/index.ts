7669825a5caa14cd06c7385eff0fa0728b3f2d73
alon galperin <alonga90@gmail.com>
2021-04-09 12:11:49 +0300

    change res

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 4000;
 
 app.get("/", (req, res) => {
-  res.send('Berg 2021!');
+  res.send('Berg 2021! update');
 });
 
 app.listen(process.env.PORT || PORT, () => {
